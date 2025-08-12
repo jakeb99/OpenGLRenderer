8e4e24075ef7d17a32dbe3dfed2f715d136c631b
Jake B <46630765+jakeb99@users.noreply.github.com>
2025-08-11 23:29:40 -0700

    add uniform name check

--- OpenGLRenderer/Shader.h
+++ OpenGLRenderer/Shader.h
@@ -86,55 +86,67 @@ public:
 	// utility uniform functions
 	void setBool(const std::string& name, bool value) const
 	{
+		checkUniformExists(name);
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 	}
 	void setInt(const std::string &name, int value) const 
 	{
+		checkUniformExists(name);
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 	}
 	void setFloat(const std::string& name, float value) const
 	{
+		checkUniformExists(name);
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
 	void setVec2(const std::string& name, const glm::vec2& value) const
 	{
+		checkUniformExists(name);
 		glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 	}
 	void setVec2(const std::string& name, float x, float y) const
 	{
+		checkUniformExists(name);
 		glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
 	}
 	// ------------------------------------------------------------------------
 	void setVec3(const std::string& name, const glm::vec3& value) const
 	{
+		checkUniformExists(name);
 		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 	}
 	void setVec3(const std::string& name, float x, float y, float z) const
 	{
+		checkUniformExists(name);
 		glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
 	}
 	// ------------------------------------------------------------------------
 	void setVec4(const std::string& name, const glm::vec4& value) const
 	{
+		checkUniformExists(name);
 		glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 	}
 	void setVec4(const std::string& name, float x, float y, float z, float w) const
 	{
+		checkUniformExists(name);
 		glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
 	}
 	// ------------------------------------------------------------------------
 	void setMat2(const std::string& name, const glm::mat2& mat) const
 	{
+		checkUniformExists(name);
 		glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
 	// ------------------------------------------------------------------------
 	void setMat3(const std::string& name, const glm::mat3& mat) const
 	{
+		checkUniformExists(name);
 		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
 	// ------------------------------------------------------------------------
 	void setMat4(const std::string& name, const glm::mat4& mat) const
 	{
+		checkUniformExists(name);
 		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
 
@@ -162,6 +174,16 @@ private:
 				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
 				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
 			}
+		}
+	}
+
+	void checkUniformExists(const std::string& name) const
+	{
+		GLint uniformLoc = glGetUniformLocation(ID, name.c_str());
+		
+		if (uniformLoc == -1)
+		{
+			std::cout << "WARNING: uniform " << name << " not found in shader with ID " << ID << std::endl;
 		}
 	}
 };
